46b0fb1478b92453822f7c52d8929cfb29d63c58
Timothy Groves <groves@greatwhiteark.com>
2017-09-17 16:34:44 +0200

    #8 Added lunr search functionality.

---
+++ plugin/lunr_index.js
@@ -0,0 +1,58 @@
+var striptags = require('striptags'),
+  marked = require('marked');
+
+var lunr_index = function (options) {
+  return function (files, metalsmith, done) {
+    var data = [],
+      key,
+      item,
+      output;
+
+    for (key in files) {
+      if (!files.hasOwnProperty(key)) {
+        continue;
+      }
+
+      item = files[key];
+
+      data.push(
+        JSON.stringify({
+          id: key.replace('.md', ''),
+          title: item.title,
+          text: getIndexText(item.contents.toString())
+        })
+      );
+    }
+
+    output = '[' + data.join(', ') + ']';
+
+    files['lunr_index.json'] = {
+      contents: output
+    };
+
+    done();
+  };
+};
+
+/**
+ * @param {String} str
+ * @return {String}
+ */
+function getIndexText(str) {
+  return replaceNewLines(
+    striptags(
+      marked(str)
+    )
+  );
+}
+
+/**
+ * @param {String} str
+ * @return {String}
+ */
+function replaceNewLines(str) {
+  return str.replace(/\n/g, ' ');
+}
+
+// Expose the plugin
+module.exports = lunr_index;
